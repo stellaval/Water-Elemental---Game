1c6b1dabc33ca35407dfba084706b8c1a682de7b
spv <stellavalcheva@yahoo.com>
2015-02-12 20:23:27 +0200

    trying to push my project :)

---
+++ Try/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try
+{
+    class Program
+    {
+        int basketPositionX=0;
+        int basketPositionY=0;
+
+        static void Main(string[] args)
+        {
+           Console.BufferHeight = Console.WindowHeight;
+           Console.BufferWidth = Console.WindowWidth;
+            while(true)
+            {
+
+            }
+        }
+    }
+}
+
+/*
+------------------------------------------------------------|
+                                                            |
+                                                            |
+\                                                          /|
+\\                                                        //|
+\\\  ()                                                  ///| 
+\\\\                                                    ////| 
+\\\\\   ()                                             /////|
+\\\\\\                                                //////|
+                                                            |
+\                                                          /|
+\\\           |       |                                  ///|
+\\\\\         |_______|                                /////|
+\\\\\\\                                              ///////|
+\\\\\\\\\                                          /////////|
+                                                            |
+                                                            |
+                                                            |
+                                                            |
+                                                            |
+------------------------------------------------------------|
+
+*/
+
